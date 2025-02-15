cf7b82d4d418f3424c5911657b406747708c6e19
kakita.media <kakita.media@gmail.com>
2025-02-15 16:15:20 +0900

    add cos_similarity loss

--- losses/freq_constraint.py
+++ losses/freq_constraint.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+import einops
 
 from losses import register, make
 
@@ -20,14 +21,24 @@ class PeriodicityLoss(nn.Module):
 
             freq = torch.stack(torch.split(freq, 6*self.block_size, dim=-1), dim=-1)
             repeated = freq[:, ]
-
-# TODO: Implement the following loss function
+@register('cos_similarity')
 class CosSimilarityLoss(nn.Module):
     def __init__(self, weight=1.0, **kwargs):
         super().__init__()
 
         self.fn = nn.CosineSimilarity(dim=-1)
         self.weight = weight
+        self.block_size = getattr(kwargs['model'].predictor, 'block_size', 1)
+        assert self.block_size > 1, 'block_size must be greater than 1'
+        self.out_dim = kwargs['model'].predictor.out_dim
 
     def forward(self, pred, gt):
-        return self.fn(pred, gt) * self.weight
+        bs, q = pred['freq'][0].shape[:2]
+        for i in range(len(pred['freq'])):
+            freq = torch.stack(torch.split(pred['freq'][i], 6*self.block_size, dim=-1), dim=-1)
+            freq = einops.rearrange(freq, '... (l xy) t -> ... l xy t', xy=2)
+            freq = torch.stack(torch.split(freq, 3, dim=-3), dim=-1).permute(0, 1, 2, 4, 3, 5)
+            freq = freq / freq.norm(dim=-2, keepdim=True)
+            loss = torch.matmul(freq.transpose(-2, -1), freq).mean()
+            loss_dict = {'loss': loss.item()}
+            return loss, loss_dict
